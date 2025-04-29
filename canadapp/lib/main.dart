bc6fa36c23db8d0e0e250bc9f060c49fc393ce65
Edoardo Di Giuseppe <edoardodg09@gmail.com>
2025-04-29 11:13:12 +0200

    struttura package

--- canadapp/lib/main.dart
+++ canadapp/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Test prova 200',
+      title: 'Firebase Test prova 200', //
       home: FirebaseTestScreen(),
     );
   }
